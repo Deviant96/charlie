423447d9d2959cea8962537c06b73b475a407e69
Miretazam Ciptaprima <Mcrealdeviant@gmail.com>
2019-08-22 03:29:55 +0700

    Update footer.php

--- application/views/_partials/footer.php
+++ application/views/_partials/footer.php
@@ -1,8 +1,10 @@
 <!-- Sticky Footer -->
-<footer class="sticky-footer">
+<footer class="page-footer font-small blue">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
             <span>Copyright © <?php echo SITE_NAME ." ". Date('Y') ?></span>
+            <p class="small">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?><br>
+			If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.	</p>
         </div>
     </div>
 </footer>

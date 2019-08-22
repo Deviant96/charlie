3a213e652b0cc006bb81194acf657f44ea004e38
Miretazam Ciptaprima <Mcrealdeviant@gmail.com>
2019-08-22 23:52:13 +0700

    Update footer.php
    Menjadi fixed footer

--- application/views/_partials/footer.php
+++ application/views/_partials/footer.php
@@ -1,6 +1,6 @@
 <!-- Sticky Footer -->
-<footer class="page-footer font-small blue">
-    <div class="container my-auto">
+<footer class="fixed-bottom">
+    <div class="container-fluid my-auto">
         <div class="copyright text-center my-auto">
             <span>Copyright © <?php echo SITE_NAME ." ". Date('Y') ?></span>
             <p class="small">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?><br>

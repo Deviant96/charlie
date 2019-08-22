6444920bb2b6d24c922dc631ee53cc62ba76e6b8
Miretazam Ciptaprima <Mcrealdeviant@gmail.com>
2019-08-22 22:59:36 +0700

    Removed Chart javascript
    Removed Chart.min.js and chart-area-demo.js

--- application/views/_partials/js.php
+++ application/views/_partials/js.php
@@ -16,7 +16,6 @@
   <script src="<?php echo base_url('assets/jquery-easing/jquery.easing.min.js')?>"></script>
 
   <!-- Page level plugin JavaScript-->
-  <script src="<?php echo base_url('assets/chart.js/Chart.min.js')?>"></script>
   <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js')?>"></script>
   <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap4.js')?>"></script>
 
@@ -25,4 +24,3 @@
 
   <!-- Demo scripts for this page-->
   <script src="<?php echo base_url('js/demo/datatables-demo.js')?>"></script>
-  <script src="<?php echo base_url('js/demo/chart-area-demo.js')?>"></script>
